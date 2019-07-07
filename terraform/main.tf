7f2a036bb68edbac6ebd47bfca2b42de3ad1542c
Nikolay Ostroushko <koxx009@gmail.com>
2019-07-07 17:57:33 +0300

    L8 - HW - Complete

---
+++ terraform/main.tf
@@ -0,0 +1,78 @@
+terraform {
+  # Версия terraform
+  required_version = "0.11.11"
+}
+
+provider "google" {
+  # Версия провайдера
+  version = "2.0.0"
+
+  # ID проекта
+  project = "${var.project}"
+  region  = "${var.region}"
+}
+
+resource "google_compute_instance" "app" {
+  name         = "reddit-app"
+  machine_type = "g1-small"
+  zone         = "${var.zone}"
+
+  # определение загрузочного диска
+  boot_disk {
+    initialize_params {
+      image = "${var.disk_image}"
+    }
+  }
+
+  # определение сетевого интерфейса
+  network_interface {
+    # сеть, к которой присоединить данный интерфейс
+    network = "default"
+
+    # использовать ephemeral IP для доступа из Интернет
+    access_config {}
+  }
+
+  metadata {
+    ssh-keys = "koxx009:${file(var.public_key_path)}"
+  }
+
+  tags = ["reddit-app"]
+
+  connection {
+    type  = "ssh"
+    user  = "koxx009"
+    agent = false
+
+    # путь до приватного ключа
+    private_key = "${file(var.private_key)}"
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+
+  # Название сети, в которой действует правило
+  network = "default"
+
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  # Каким адресам разрешаем доступ
+  source_ranges = ["0.0.0.0/0"]
+
+  # Правило применимо для инстансов с перечисленными тэгами
+  target_tags = ["reddit-app"]
+}
