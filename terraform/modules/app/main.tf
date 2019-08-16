fa505d91aaf68df5d82dc670c0bb86bf7bde4293
Nikolay Ostroushko <koxx009@gmail.com>
2019-08-17 01:06:12 +0300

    HW 12. Complete

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -40,3 +40,18 @@ resource "google_compute_firewall" "firewall_puma" {
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["reddit-app"]
 }
+
+resource "google_compute_firewall" "firewall_puma_http" {
+  name    = "allow-puma-http"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+
+    ports = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["reddit-app"]
+}
+
